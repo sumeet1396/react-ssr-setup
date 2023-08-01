7696ec23eb21145818d245d1bba99c2de9a64474
sumeet <sumeet1396@gmail.com>
2023-08-01 21:40:57 +0530

    rendere function created

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import React from 'react'; 
-import { renderToString } from 'react-dom/server';
-import Home from './Client/Page/Home';
+import rendere from './helpers/rendere';
 
 const app = express();
 
@@ -12,17 +10,8 @@ const port = process.env.PORT;
 
 app.use(express.static('public'));
 app.get("/", (req, res) => {
-    const content = renderToString(<Home />);
-    const html = `
-    <html>
-      <head></head>
-      <body>
-        <div id="root">${content}</div>
-        <script src="bundle.js"></script>
-      </body>
-    </html>
-    `;
-    res.send(html);
+    
+    res.send(rendere());
 })
 
 app.listen(port, () => {
